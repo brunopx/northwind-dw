1be6b77cc3955f8dedd1fb13c5e1569271bd8692
brunopeixoto.tucano@gmail.com <brunopeixoto.tucano@gmail.com>
2023-12-26 22:58:56 +0000

    criei stgs produtos e fornecedores e a dim produtos

---
+++ models/staging/erp_northwind/stg_erp__produtos.sql
@@ -0,0 +1,17 @@
+WITH 
+    fonte_produtos AS (
+        SELECT 
+            CAST(product_id AS INT) AS id_produto
+            ,CAST(supplier_id AS INT) AS id_fornecedor
+            ,CAST(category_id AS INT) AS id_categoria 
+            ,CAST(product_name AS STRING) AS nome_produto
+            ,CAST(quantity_per_unit AS STRING) AS quantidade_por_unidade
+            ,CAST(unit_price AS DECIMAL) AS preco_por_unidade
+            ,CAST(units_in_stock AS INT) AS unidades_em_estoque
+            ,CAST(units_on_order AS INT) AS unidades_por_ordem
+            ,CAST(reorder_level AS INT) AS nivel_reabastecimento
+            ,CASE WHEN discontinued = 1 THEN TRUE ELSE FALSE END AS is_descontinuado
+        FROM {{ source('erp', 'products') }}
+    )
+
+SELECT * FROM fonte_produtos
